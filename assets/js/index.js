6ae656370274e82e08329f420f95d9687093275f
noteseba <berendicto@gmail.com>
2023-06-12 16:30:16 -0400

    Segunda parte del desafio lista, se agregan las condiciones y la funcion on click

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,4 +12,25 @@ document.addEventListener("click", (e) => {
     }
 })
 
+// Segundo challenge, lista de items que deben indicar si la cantidad supera los 10 y sino es tal, indicar cuantos lleva. //
+
+function calculate(){
+    let diablo4 = document.getElementById("diablo4").value;
+    let wow = document.getElementById("wow").value;
+    let starcraft = document.getElementById("starcraft").value;
+    
+    let resultado = Number(diablo4) + Number(wow) + Number(starcraft);
+    
+    if (resultado <= 10) {
+        document.getElementById("checkresults").innerHTML=
+        "You have " + resultado + " codes"
+        resultado;
+       
+    } else {
+      document.getElementById("checkresults").innerHTML=
+        "Thats way too much!";
+    }
+    }
+    
+
 
